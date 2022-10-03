6ce478d21492ce86da19a10c3e4fa67fc1a93ac8
Laura OwO <88065630+Artixis@users.noreply.github.com>
2022-10-03 16:07:30 +1100

    Delete Mathematics Project directory

--- Mathematics Project/codeChunk.R
+++
@@ -1,8 +0,0 @@
-
-## ---- myrcode2
-df_pop = read.csv("C:\\Users/Laura/OneDrive - Western Sydney University/Spring 2022/Mathematics Project/Sets Modified/Population_done.csv")
-normData  = rnorm(n = 100, mean = 0, sd = 1)
-par(mfrow=c(1,2))
-
-hist(normData, main="Noraml Distribution", xlab="Data")
-hist(df_pop$distance_from_syd, main="Our Data", xlab="Data")
